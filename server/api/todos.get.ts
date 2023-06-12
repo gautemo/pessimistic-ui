363bc41997601d35d190915233e0b80b2a4a04a4
Gaute Meek Olsen <gaute.meek@gmail.com>
2023-06-12 18:28:30 +0200

    update storage base

--- server/api/todos.get.ts
+++ server/api/todos.get.ts
@@ -3,6 +3,6 @@ import { sleep } from "../sleep"
 export default defineEventHandler(async (event) => {
   const userId = getHeader(event, 'user-id')
   await sleep(2000)
-  const saved = await useStorage().getItem(`${userId}:todos`)
+  const saved = await useStorage(userId).getItem('todos')
   return saved ?? []
 })
